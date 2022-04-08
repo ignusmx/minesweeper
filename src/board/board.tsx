44645ddb19b4d1324932ad7086f0ff0d9fd5a1bc
Christian Læirbag <laeirbag@gmail.com>
2022-04-08 14:55:11 -0600

    Fixes

--- src/board/board.tsx
+++ src/board/board.tsx
@@ -5,25 +5,11 @@ import {addMessage} from "./../control/actions"
 
 const Board = ({messages} : any)=>
 {
-	/*console.log(messages);
+	console.log(messages);
 
 	return <section id="display">
 		<Message{...messages[messages.length - 1]}/>
-	</section>*/
-	
-	const currentIndex = messages.length - 1
-
-	if(currentIndex < 0)
-	{
-		return <section id="display"></section>
-	}
-	else
-	{
-		switch(messages[messageCount - 1])
-		{
-			
-		}
-	}
+	</section>
 }
 
 Board.propTypes=
